461fa1afd32c8c125f747f7256a87ffe950d152b
VANI-0906 <vani.narayanaswamy@accenture.com>
2024-07-25 19:54:04 +0530

    Register & Authentication

--- Flight Asset/myproject/users/urls.py
+++ Flight Asset/myproject/users/urls.py
@@ -1,11 +1,12 @@
 from django.urls import path
 from .views import RegisterView
-from .views import LoginView
+from .views import LoginView, RefreshTheAccessToken
 
  
 urlpatterns = [
     path('register/', RegisterView.as_view(), name='register'),
     path('login/', LoginView.as_view(), name='token_obtain_pair'),
+    path('refresh/', RefreshTheAccessToken.as_view(), name='refresh'),
 
 ]
 
--- Flight Asset/myproject/users/views.py
+++ Flight Asset/myproject/users/views.py
@@ -40,12 +40,30 @@ class LoginView(APIView):
         refresh = RefreshToken.for_user(user)
         return Response({
             'refresh_token': str(refresh),
-            'refresh_token_validlity': "valid for 1 day",
+            'refresh_token_validlity': "(valid for 1 day)",
             'access_token': str(refresh.access_token),
-            'access_token_validity': "valid for 10 minutes"
+            'access_token_validity': "(valid for 10 minutes)"
         })
 
 
+class RefreshTheAccessToken(APIView):
+    def post(self, request):
+        refresh_token = request.data.get('refresh_token')
+
+        if not refresh_token:
+            return Response({'error': 'Refresh token is required'}, status=status.HTTP_400_BAD_REQUEST)
+
+        try:
+            refresh = RefreshToken(refresh_token)
+            new_access_token = str(refresh.access_token)
+
+            return Response({
+                'access_token': new_access_token,
+                'access_token_validity': "(valid for 10 minutes)"
+            }, status=status.HTTP_200_OK)
+
+        except Exception as e:
+            return Response({'error': str(e)}, status=status.HTTP_400_BAD_REQUEST)
 
 
 
